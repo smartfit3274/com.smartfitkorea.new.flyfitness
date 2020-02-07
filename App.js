fe540b9b7c85406a9e9dae8bf72437c92b1eb3ed
dave no <daveno@naver.com>
2020-02-07 18:13:19 +0900

    init

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Text,
+} from 'react-native';
+import styled from 'styled-components';
+
+function App() {
+  return (
+    <Text>aaaa</Text>
+  );
+};
+
+export default App;
