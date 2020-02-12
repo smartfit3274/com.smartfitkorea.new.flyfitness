f11fddf4dd032a8dab20392c1cf93008d9098145
dave no <daveno@naver.com>
2020-02-12 18:28:22 +0900

    메인화면진행

--- App.js
+++ App.js
@@ -1,19 +1,44 @@
 import React from 'react';
 import {
   View,
-  Text
+  Text,
+  SafeAreaView,
+  Image,
+  Dimensions,
+  StyleSheet,
 } from 'react-native';
 import styled from 'styled-components/native';
 
+const Container = styled.View`
+  flex:1;
+  background:#ecf0f1;
+  align-items:center;
+`;
+
 const MainText = styled.Text`
-  font-size:20;
-  color: red;
+  font-size:25;
+  margin-top:50px;
+  text-align:center;
+`;
+
+const Logo = styled.Image`
+  width: ${Dimensions.get('window').width * 0.2}px;
+  background: red;
 `;
 
 export default function App() {
+
   return (
     <View>
-      <MainText>aaaa</MainText>
+      <SafeAreaView>
+        <Container>
+          <MainText>스마트패스 출입시스템</MainText>
+          <Logo
+          source={require('./components/images/logo.png')}
+          resizeMode="contain"
+          ></Logo>
+        </Container>
+      </SafeAreaView>
     </View>
   );
 };
