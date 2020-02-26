c36d768086b080c3272c3ddbf5b497ba4d8e2b26
dave no <daveno@naver.com>
2020-02-26 18:21:06 +0900

    회원가입

--- components/JoinScreen1.js
+++ components/JoinScreen1.js
@@ -10,15 +10,15 @@ import {
   ScrollView,
   Keyboard,
   TouchableOpacity,
-  Text,
   Alert,
+  Text,  
 } from 'react-native';
 import styled from 'styled-components/native';
 import { useNavigation } from 'react-navigation-hooks'
-import Icon from 'react-native-vector-icons/FontAwesome'
 import { CheckBox, Input
 } from "react-native-elements";
 import axios from 'axios';
+import Icon from 'react-native-vector-icons/dist/FontAwesome';
 
 const Container = styled.View`
   flex:1;
@@ -27,7 +27,7 @@ const Container = styled.View`
 
 const SubContainer = styled.View`
   flex:1;
-  width: 80%;
+  
   /* border:2px solid green; */
 `;
 
@@ -58,29 +58,71 @@ const ButtonAgree = styled.TouchableOpacity`
 
 export default function JoinScreen1(props) {
 
+  // useEffect( async () => {
+  //   await Font.loadAsync({
+  //     Roboto: require('native-base/Fonts/Roboto.ttf'),
+  //     Roboto_medium: require('native-base/Fonts/Roboto_medium.ttf'),
+  //     ...Ionicons.font,
+  //   });
+  //   console.log('[TAG] Font ready!');
+  // },[]);
+
+  const mode = "http";
   const { navigate, goBack }  = useNavigation();
   const phoneRef = useRef();
-  const mode = "http";
+  const nameRef = useRef();
+  const passRef = useRef();
+  const passConfirmRef = useRef();  
+  const [BtnAgreeDisabled, SetBtnAgreeDisabled] = useState(false);
+
+  
 
   function BtnClose() {
     console.log('TAG - goback();');
   }
 
   function BtnOk() {
-    console.log('TAG - ok();');
+
+
+    console.log('[TAG] BtnOk()');
+    console.log('BtnAgreeDisabled',BtnAgreeDisabled);
+    SetBtnAgreeDisabled(true);
+
+
     let phone = phoneRef.current.input._lastNativeText;
+    let name = nameRef.current.input._lastNativeText;
+    let pass = passRef.current.input._lastNativeText;
+    let passConfirm = passConfirmRef.current.input._lastNativeText;   
+
+
     if(phone== undefined) {
       phone = '';
     }
-    console.log('TAG phone', phone);
+    if(name == undefined) {
+      name = '';
+    }
+    if(pass == undefined) {
+      pass = '';
+    }
+    if(passConfirm == undefined) {
+      passConfirm = '';
+    }    
     
     let url = "";
     if( mode == "https") url = "https://pass.smartg.kr/rest/join1";
     if( mode == "http") url = "http://192.168.76.51/rest/join1";
+    
     console.log(url);
 
-    axios.get(url)
-    .then(function(res){
+    let data = {
+      phone:phone,
+      name:name,
+      pass:pass,
+      passConfirm:passConfirm,
+    }
+
+    axios.post(url, data)
+    .then(function(res){      
       if(res.data.ret=='N')
       {
         Alert.alert(
@@ -89,107 +131,85 @@ export default function JoinScreen1(props) {
             [{text:'ok',onPress:()=>console.log('OK pressed')}],
             {
               cancelable:false,
-            }        
+            }
         );
-        
-
       }
+
+      SetBtnAgreeDisabled(false);      
     })
     .catch(function(e){
       console.log(e);
-    })
-
-    // fetch('http://127.0.0.1/rest/join1',{
-    //   method:'get',
-    //   header: {
-    //     'Accept': 'application/json',
-    //     'Content-type' : 'application/json'
-    //   }, 
-    //   body : JSON.stringify({
-    //     phone:phone
-    //   })
-    // })
-    // .then ((res)=> {
-    //   console.log('then()')
-    // })
-    // .catch((err)=>{
-    //   console.log (err);
-    // })
-
-    // console.log('TAG - value', phoneRef.current.value);
-    // JOIN1 POST
-    /*
-    fetch('http://127.0.0.1/rest/join1',{
-			method:'post',
-			header:{
-				'Accept': 'application/json',
-				'Content-type': 'application/json'
-			},
-			body:JSON.stringify({
-				email: userEmail,
-				password: userPassword
-			})			
-    })
-    */
+    });  
 
   }  
 
   return (  
     <Container>
       <SubContainer>
-        <ScrollView>
+          <ScrollView style={{paddingRight:10,paddingLeft:10}}>        
           <Title>회원정보를 입력하세요.</Title>
           <Input
-          ref = {phoneRef}
+          idx="1"
+          ref={phoneRef}
           containerStyle={{ marginTop: 30}}
           leftIcon={{ type: 'font-awesome', name: 'phone', color:'#cccccc' }}
           errorStyle={{ color:'red'}}
           errorMessage=''
-          label='휴대폰 번호 (Phone Number)'        
+          label='휴대폰 번호 (Phone Number) / - 없이입력'        
           inputStyle={{ paddingLeft:15}}
-          placeholder=' - 없이 입력'
-          keyboardType='numeric'          
+          placeholder=''
+          keyboardType='numeric'   
           ></Input>
 
           <Input
+          ref={nameRef}
           containerStyle={{ marginTop: 30}}
           leftIcon={{ type: 'font-awesome', name: 'user', color:'#cccccc' }}
           errorStyle={{ color:'red'}}
           errorMessage=''
-          label='이름 (Name)'
+          label='이름 (Name) / 실명'
           inputStyle={{ paddingLeft:15}}
           ></Input>
 
           <Input
+          ref={passRef}
           containerStyle={{ marginTop: 30}}
           leftIcon={{ type: 'font-awesome', name: 'lock', color:'#cccccc' }}
           errorStyle={{ color:'#000'}}
           errorMessage=''
-          label='비밀번호 (Password)'
+          label='비밀번호 (Password) / 영문+숫자 (4~8자리)'
           inputStyle={{ paddingLeft:15}}
-          placeholder='영문 + 숫자 (4~8자리)'
+          placeholder=''
+          secureTextEntry={true}       
           ></Input>            
 
           <Input
+          ref={passConfirmRef}
           containerStyle={{ marginTop: 30}}
           leftIcon={{ type: 'font-awesome', name: 'lock', color:'#cccccc' }}
           errorStyle={{ color:'#000'}}
           label='비밀번호 확인 (Password Confirm)'
           inputStyle={{ paddingLeft:15}}
+          secureTextEntry={true}
           ></Input>
-        </ScrollView>
+          </ScrollView>
       </SubContainer>
 
       <BottomContainer>
         <ButtonNotAgree>
           <Text onPress={()=>BtnClose()}style={{color:'#fff'}}>이전</Text>
         </ButtonNotAgree>
-        <ButtonAgree>
-          <Text onPress={()=>BtnOk()}style={{color:'#fff'}}>다음</Text>
-        </ButtonAgree>
+        <ButtonAgree
+        onPress={()=>BtnOk()}
+        disabled={BtnAgreeDisabled}
+        >
+          <Text style={{color:'#fff'}}>다음</Text>
+        </ButtonAgree>        
       </BottomContainer>
       
     </Container>
   );
 
 };
+
+// style={{ backgroundColor:BtnAgreeDisabled==false?'#2980b9':'#3498db' }}
