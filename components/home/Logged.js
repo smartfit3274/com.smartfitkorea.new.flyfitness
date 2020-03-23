000a470c3c9d62d90e6c887978053100f422b69f
dave no <daveno@naver.com>
2020-03-23 18:39:08 +0900

    바이오 인증

--- components/home/Logged.js
+++ components/home/Logged.js
@@ -10,12 +10,19 @@ import {
     Body,
     Title,
     Content,
-    Button    
+    Button
 } from 'native-base';
-import Icon from 'react-native-vector-icons/MaterialIcons';
-
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Image,Dimensions } from 'react-native';
 
 function Logged() {
+
+    const window = Dimensions.get('window');
+
+    function open_door() {
+        console.log('open door!!!');
+    }
+
     return (
       <>
         <Header>
@@ -25,22 +32,35 @@ function Logged() {
                 </Button>
             </Left>
             <Body style={{flex:1,justifyContent:"center"}}>
-                <Title style={{alignSelf:"center"}}>SmartGYM</Title>
-                
+                <Image source={require('../images/logo_smartgym.jpg')} style={{width:150, height:35, alignSelf:"center"}}></Image>
             </Body>
-            <Right style={{flex:1}}>
-                <Button transparent onPress={()=>console.log('account pressed!')}>
-                    <Icon name="account-circle" style={{color:"white", fontSize:30}}></Icon>
-                </Button>
-            </Right>
+            <Right style={{flex:1}}></Right>
         </Header>
 
-        <Content>
-            <Text>본문</Text>
+        <Content scrollEnabled={false}>
+            <Image source={require('../images/bg.jpg')}
+            style={{ width: window.width, height: window.height}}            
+            ></Image>
         </Content>
-        
+
         <Footer>
             <FooterTab>
+                <Button vertical>
+                    <Image source={require('../images/mypage_btn.png')} style={{width:25, height:25}}></Image>
+                    <Text>내 정보</Text>
+                </Button>
+                <Button vertical>
+                <Image source={require('../images/card_btn.png')} style={{width:25, height:25}}></Image>
+                    <Text>카드결제</Text>
+                </Button>  
+                <Button vertical>   
+                    <Image source={require('../images/fingerprint_btn.png')} style={{width:25, height:25}}></Image>
+                    <Text>지문등록</Text>
+                </Button>           
+                <Button vertical onPress={()=>open_door()}>
+                    <Image source={require('../images/power-off-btn.png')} style={{width:25, height:25}}></Image>
+                    <Text>문열기</Text>
+                </Button>
             </FooterTab>
         </Footer>        
       </>      
