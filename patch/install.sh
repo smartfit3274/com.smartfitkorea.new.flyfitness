6a204e42920ce4db92fe560ef2a9ce8d27f3f25f
dave no <daveno@naver.com>
2020-06-18 17:42:32 +0900

    나의정보 수정

--- patch/install.sh
+++ patch/install.sh
@@ -1,2 +1,6 @@
+#ios
 cp react-native-beacons-manager.podspec ../node_modules/react-native-beacons-manager
 cp RNiBeacon.m ../node_modules/react-native-beacons-manager/ios/RNiBeacon
+
+#android
+cp build.gradle ../node_modules/react-native-beacons-manager/android
