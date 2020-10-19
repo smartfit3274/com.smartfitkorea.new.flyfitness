03f132a7dfd15e4407ac9d0e5bd8ba23fa2456b2
dindoo <dindoo0524@gmail.com>
2020-10-19 11:42:46 +0900

    임시 ios id 1.6.3 배포

--- store.js
+++ store.js
@@ -17,9 +17,9 @@ const INITIAL_STATE = {
     debug_beacon_distance : "N",
     debug_beacon_console : "N",
     debug_door : "33",
-    app_version : "1.6.2",
+    app_version : "1.6.3",
     store_id_android: "com.smartfitkorea.metroxi",
-    store_id_ios : "1534844508",
+    store_id_ios : "1397721927",
     iamport:"imp76426114",
     tier_code:"G03"    
   }
